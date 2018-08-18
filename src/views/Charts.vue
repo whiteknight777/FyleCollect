95d97f9ab41e0c3fc01b52cf5ef8c937cca48497
Désire ARRA <guymartial.arra@fyle-group.com>
2018-08-18 23:36:24 +0000

    Creation du composant Chart et update de la page d'accueil

---
+++ src/views/Charts.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- <div> -->
+    <div style="padding:10px">
+    <section>
+      <span style="font-size: 17px;text-transform: uppercase;"> {{legend}} </span>
+    </section>
+      <canvas id="chart"></canvas>
+    </div>
+  <!-- </div> -->
+</template>
+
+
+<script>
+export default {
+  name: "Charts",
+  props: ["data", "type", "options", "legend"],
+  mounted() {
+    this.createChart(this.data, this.type, this.options);
+  },
+  methods: {
+    createChart(chartId, chartData) {
+      const ctx = document.getElementById("chart");
+      const myChart = new Chart(ctx, {
+        type: this.type,
+        data: this.data,
+        options: this.options
+      });
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,103 @@
   <v-container fluid grid-list-md>
     <v-slide-y-transition mode="out-in">
       <v-container grid-list-md text-xs-center>
-        <v-layout row wrap>
   
-  
-  
-  
-  
-          <v-flex xs9>
-            <v-card style="height: 441px">
-              <canvas id="planet-chart"></canvas>
+        <v-layout row wrap style="margin-bottom:10px">
+          <!--  Stat bar -->
+          <v-flex xs3>
+            <v-card>
+              <div class="card-icon-block warning" dark>
+                <v-icon dark>sort</v-icon>
+              </div>
+              <v-card-title style="padding-top: 50px;padding-bottom:0">
+                <div style="text-align: right;width:100%">
+                  <span class="number-card">900</span><br>
+                  <span style="font-size:16px">Total de votes acquis</span>
+                  <!-- <span>Whitsunday Island, Whitsunday Islands</span> -->
+                </div>
+              </v-card-title>
+              <v-card-actions>
+                <v-progress-linear color="success" height="4" value="15"></v-progress-linear>
+              </v-card-actions>
             </v-card>
           </v-flex>
+          <!-- Fin stat bar -->
   
+          <!--  Stat bar -->
+          <v-flex xs3>
+            <v-card>
+              <div class="card-icon-block info" dark>
+                <v-icon dark>sort</v-icon>
+              </div>
+              <v-card-title style="padding-top: 50px;padding-bottom:0">
+                <div style="text-align: right;width:100%">
+                  <span class="number-card">900</span><br>
+                  <span style="font-size:16px">Total de votes acquis</span>
+                  <!-- <span>Whitsunday Island, Whitsunday Islands</span> -->
+                </div>
+              </v-card-title>
+              <v-card-actions>
+                <v-progress-linear color="success" height="4" value="15"></v-progress-linear>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+          <!-- Fin stat bar -->
+  
+          <!--  Stat bar -->
+          <v-flex xs3>
+           <v-card>
+              <div class="card-icon-block success" dark>
+                <v-icon dark>sort</v-icon>
+              </div>
+              <v-card-title style="padding-top: 50px;padding-bottom:0">
+                <div style="text-align: right;width:100%">
+                  <span class="number-card">900</span><br>
+                  <span style="font-size:16px">Total de votes acquis</span>
+                  <!-- <span>Whitsunday Island, Whitsunday Islands</span> -->
+                </div>
+              </v-card-title>
+              <v-card-actions>
+                <v-progress-linear color="success" height="4" value="15"></v-progress-linear>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+          <!-- Fin stat bar -->
+  
+          <!--  Stat bar -->
+          <v-flex xs3>
+             <v-card>
+              <div class="card-icon-block primary">
+                <v-icon dark>sort</v-icon>
+              </div>
+              <v-card-title style="padding-top: 50px;padding-bottom:0">
+                <div style="text-align: right;width:100%">
+                  <span class="number-card">900</span><br>
+                  <span style="font-size:16px">Total de votes acquis</span>
+                  <!-- <span>Whitsunday Island, Whitsunday Islands</span> -->
+                </div>
+              </v-card-title>
+              <v-card-actions>
+                <v-progress-linear color="success" height="4" value="15"></v-progress-linear>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+          <!-- Fin stat bar -->
+  
+        </v-layout>
+
+        <v-layout row wrap>
+          <!--  Graphique -->
+          <v-flex xs9>
+            <v-card>
+              <charts :data="chartData.data" :type="chartData.type" :options="chartData.options" :legend="chartData.legend" :width="400"
+              style="height:440px">
+              </charts>
+  
+            </v-card>
+          </v-flex>
+          <!-- Fin Graphique -->
+  
+          <!-- Classement candidat -->
           <v-flex xs3>
             <v-card>
   
@@ -47,13 +132,15 @@
                     <v-icon :color="item.active ? 'teal' : 'grey'">chat_bubble</v-icon>
                   </v-list-tile-action>
                 </v-list-tile>
-                
+  
                 <v-subheader>Voir tout le classement</v-subheader>
               </v-list>
             </v-card>
           </v-flex>
+          <!-- Fin Classement candidat -->
   
         </v-layout>
+  
       </v-container>
     </v-slide-y-transition>
   </v-container>
@@ -61,9 +148,13 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <script>
-import planetChartData from "../charts/planet-charts.js";
+import charts from "./Charts.vue";
+
 export default {
   name: "Home",
+  components: {
+    charts
+  },
   data() {
     return {
       items: [
@@ -92,21 +183,57 @@ export default {
           avatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg"
         }
       ],
-      planetChartData: planetChartData
+      chartData: {
+        legend: "Graphique des votes par bureau de vote",
+        type: "line",
+        data: {
+          labels: [
+            "Bureau 1",
+            "Bureau 2",
+            "Bureau 3",
+            "Bureau 4",
+            "Bureau 5",
+            "Bureau 6",
+            "Bureau 7",
+            "Bureau 8"
+          ],
+          datasets: [
+            {
+              // another line graph
+              label: "Vous",
+              data: [4.8, 12.1, 12.7, 6.7, 139.8, 116.4, 50.7, 49.2],
+              backgroundColor: [
+                "rgba(71, 183,132,.5)", // Green
+                "rgba(71, 183,132,.5)", // Green
+                "rgba(71, 183,132,.5)", // Green
+                "rgba(71, 183,132,.5)", // Green
+                "rgba(71, 183,132,.5)", // Green
+                "rgba(71, 183,132,.5)", // Green
+                "rgba(71, 183,132,.5)", // Green
+                "rgba(71, 183,132,.5)" // Green
+              ],
+              borderColor: ["#47b784"],
+              borderWidth: 3
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          lineTension: 1,
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                  padding: 26
+                }
+              }
+            ]
+          }
+        }
+      }
     };
-  },
-  mounted() {
-    this.createChart("planet-chart", this.planetChartData);
-  },
-  methods: {
-    createChart(chartId, chartData) {
-      const ctx = document.getElementById(chartId);
-      const myChart = new Chart(ctx, {
-        type: chartData.type,
-        data: chartData.data,
-        options: chartData.options
-      });
-    }
   }
 };
 </script>
@@ -129,5 +256,19 @@ li {
 
 a {
   color: #42b983;
+}
+
+.card-icon-block {
+  position: absolute;
+  left: 15px;
+  padding: 14px;
+  top: -10px;
+  box-shadow: 2px 2px #ddd;
+}
+
+.number-card {
+  color: #4caf50 !important;
+  font-size: 30px;
+  font-weight: 600;
 }
 </style>
