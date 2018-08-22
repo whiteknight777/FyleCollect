6fa7ab2c15557dd2cf1218fab677d0ba16171cce
Désire ARRA <guymartial.arra@fyle-group.com>
2018-08-22 12:53:36 +0000

    Installation de Axios et VueX pour la gestion de l'authentification

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import Chart from 'chart.js';
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   Chart,
+  store,
   render: h => h(App)
 }).$mount('#app')
